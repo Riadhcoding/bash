68e18c329e81ba17454336c775d875c267e5cbd5
python life <71737254+python-life@users.noreply.github.com>
2020-12-13 21:09:52 +0000

    Update bash.py

--- bash.py
+++ bash.py
@@ -18,27 +18,33 @@ logo = """
 \033[1;33m   88 88 88     88ooood8
 \033[1;37m   88 88 88     88 \033[1;31m    Created by Riad\t\033[1;36m
 \033[1;34m   dP dP dP     `88888P\n"""
-def list():
-    print('\033[1;36m+++++++++++++++++++++++++++++++++++++++++++++++++++++++')
-    print('\033[1;31m[1] \033[1;32mEncrypt \033[1;31mBash Shell')
-    print('\033[1;31m[2] \033[1;32mEncryptDecode \033[1;31mBash Shell')
-    print('\033[1;31m[3] \033[1;32mYouTube')
-    print('\033[1;31m[4] \033[1;32mInstagram')
-    print('\033[1;31m[0] Exit')
-    print('\033[1;36m+++++++++++++++++++++++++++++++++++++++++++++++++++++++')
-    
+
 def clear():
     system('clear')
-def cod():
+clear()
+print(logo)
+print('\033[1;36m+++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+print('\033[1;31m[1] \033[1;32mEncrypt \033[1;31mBash Shell')
+print('\033[1;31m[2] \033[1;32mEncryptDecode \033[1;31mBash Shell')
+print('\033[1;31m[3] \033[1;32mYouTube')
+print('\033[1;31m[4] \033[1;32mInstagram')
+print('\033[1;31m[0] Exit')
+print('\033[1;36m+++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+choose = int(input('\n\033[1;31m[?] \033[1;3mChoose :\033[1;31m'))
+if choose == 1:
     clear()
     print(logo)
-    ask1 = input('\033[1;32mFile >\033[1;31m')
+    print('\033[1;36m+++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+    ask1 = input('\n\033[1;32mEncrypt File >\033[1;31m')
     enc = 'bash-obfuscate ' + ask1 + ' -o enc.sh'
     system(enc)
-def dec():
+    print('\033[1;31mEncryption successful')
+##########################################################"
+elif choose == 2:
     clear()
     print(logo)
-    file = input('\033[1;32mEncryptFile >\033[1;31m')
+    print('\033[1;36m+++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+    file = input('\n\033[1;32m Decoding File >\033[1;31m')
     openfile = open(file, 'r')
     readfile = openfile.read()
     openfile.close()
@@ -52,19 +58,11 @@ def dec():
     remove(outfile)
     system('mv -f tools.sh ' + outfile)
     print('success decrypt | file save as ' + outfile)
-clear()
-print(logo)
-list()
-choose = input('\n\033[1;31m[?] \033[1;3mChoose :\033[1;31m')
-if choose == 1:
-    cod()
-elif choose == 2:
-    dec()
 elif choose == 3:
     system('xdg-open https://www.youtube.com/c/pythonlife')
-elif choose ==4:
+elif choose == 4:
     system('xdg-open http://www.instagram.com/python.life')
 elif choose == 0:
     system('exit')
 else:
-    print('\033[1;31mplease choose 1 or 2 or 3 or 4 or 0 for exit '.upper())
+    print('please choose 1 or 2 or 3 or 4 or 0 for exit'.upper())
